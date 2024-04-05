14e29c33d76d17ccfc222942266d0cefa820714b
Shantilal Patliya <shantilalpatliya@outlook.com>
2024-04-05 23:55:55 +0530

    'varibles'

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	var firstName string = "Shantilal"
+	var lastName = "Patliya"
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+
+	var age int = 26
+	var phoneNumber = 9784102014
+
+	fmt.Println(age)
+	fmt.Println(phoneNumber)
+
+	var height float64 = 5.6
+
+	fmt.Println(height)
+
+	var isMarried bool = false
+
+	fmt.Println(isMarried)
+
+	const pi = 45
+
+	fmt.Println(pi)
+
+	// shortcut to declare variable
+
+	person := 123
+	fmt.Println(person)
+
+	var Public = "data is important"
+	var private = "data is private"
+
+	fmt.Println(Public)
+	fmt.Println(private)
+
 }
---
+++ myutil/myutil.go
@@ -0,0 +1,7 @@
+package myutil
+
+import "fmt"
+
+func PrintMessage(msg string) {
+	fmt.Println(msg)
+}
